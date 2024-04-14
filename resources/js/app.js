332595841a630a2b862dddfe80884014c0e78562
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-04-14 21:14:04 +0630

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import './bootstrap';
+
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
+livewire_hot_reload();
+
+import intersect from '@alpinejs/intersect'
+Alpine.plugin(intersect)
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+window.Swiper = Swiper;
+window.Navigation = Navigation;
+window.Pagination = Pagination;
+
