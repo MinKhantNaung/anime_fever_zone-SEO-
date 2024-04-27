1fceb33af7c0cd02b158183c3a80c1b162d04b50
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-04-27 21:55:31 +0630

    increment view count

--- app/Livewire/PostShow.php
+++ app/Livewire/PostShow.php
@@ -19,9 +19,15 @@ class PostShow extends Component
             ->get();
 
         $this->post = Post::with('media', 'topic', 'tags', 'sections', 'comments')
-            ->select('id', 'topic_id', 'heading', 'body', 'created_at')
+            ->select('id', 'topic_id', 'heading', 'body', 'view', 'created_at')
             ->where('slug', $this->slug)
             ->first();
+
+        if ($this->post) {
+            // Increment view count
+            $this->post->view++;
+            $this->post->save();
+        }
     }
 
     public function render()
