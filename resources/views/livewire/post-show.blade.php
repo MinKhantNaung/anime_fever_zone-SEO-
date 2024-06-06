b28304e6d32b3521395265c9426ab5f6bc4102da
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-06-06 16:51:26 +0630

    Update: remove  alert message in post-show

--- resources/views/livewire/post-show.blade.php
+++ resources/views/livewire/post-show.blade.php
@@ -2,12 +2,6 @@
     {{ substr($post->body, 0, 150) }}...
 @endsection
 
-@if(session('success'))
-    <div>
-        Thanks, please check your inbox to confirm subscription
-    </div>
-@endif
-
 <div class="container mx-auto flex flex-wrap py-6">
 
     <section class="w-full md:w-2/3 flex flex-col items-center px-3">
