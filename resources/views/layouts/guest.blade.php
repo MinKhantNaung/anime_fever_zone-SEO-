dea89c931c67314aa3ceba5cef7f36d4827049dc
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-06-25 17:47:11 +0630

    Refactor: add nav-bar in guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
 
-        <x-nav-bar />
+        <livewire:components.nav-bar />
 
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
