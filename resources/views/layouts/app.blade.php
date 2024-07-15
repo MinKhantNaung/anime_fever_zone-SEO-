2675e80464646785015e0fdc91f636ee91d5be88
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-15 21:12:36 +0630

    Fix: remove fonts links in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,14 @@
 
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     <link rel="canonical" href="{{ url()->current() }}">
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <style>
         [x-cloak] {
             display: none !important;
         }
     </style>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
