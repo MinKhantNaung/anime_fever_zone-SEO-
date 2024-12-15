4140d651b7c8440b5e22362834be4e6a401ce2b1
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-12-15 23:13:21 +0630

    Fix: img alt show for avoid redundant text.

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -28,7 +28,7 @@ the latest trends and discussions across a wide range of topics." />
             <div class="grid grid-cols-12 gap-1 bg-white shadow my-4">
                 <div class="col-span-12 lg:col-span-5">
                     <a wire:navigate href="{{ route('post', $post->slug) }}">
-                        <img src="{{ $post->media->url }}" alt="{{ $post->heading }}" class="w-full object-cover">
+                        <img src="{{ $post->media->url }}" alt="Image representing {{ $post->heading }}" class="w-full object-cover">
                     </a>
                 </div>
 
