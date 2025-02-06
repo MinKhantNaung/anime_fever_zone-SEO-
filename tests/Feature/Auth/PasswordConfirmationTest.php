697c563ff0a52e31181738630bebb5d16f377139
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 10:26:08 +0630

    Refactor: redirect route in PasswordConfirmationTest

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -34,7 +34,7 @@ class PasswordConfirmationTest extends TestCase
         $component->call('confirmPassword');
 
         $component
-            ->assertRedirect('/dashboard')
+            ->assertRedirect('/')
             ->assertHasNoErrors();
     }
 
