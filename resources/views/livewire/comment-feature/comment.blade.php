bab062e2833b9f33e3bb647246cd0cd3463c0fff
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 15:39:21 +0630

    Update: show profile image in comment

--- resources/views/livewire/comment-feature/comment.blade.php
+++ resources/views/livewire/comment-feature/comment.blade.php
@@ -14,7 +14,7 @@
 
                 <div class="flex items-center">
                     <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
-                            class="mr-2 w-6 h-6 rounded-full" src="{{ $comment->user->avatar() }}"
+                            class="mr-2 w-6 h-6 rounded-full" src="{{ $comment->user->media ? $comment->user->media->url : $comment->user->avatar() }}"
                             alt="{{ $comment->user->name }}">{{ Str::ucfirst($comment->user->name) }}</p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">
                         <time pubdate datetime="{{ $comment->presenter()->relativeCreatedAt() }}"
