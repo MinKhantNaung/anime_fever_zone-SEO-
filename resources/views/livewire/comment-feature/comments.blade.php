2bb69a8c45a2487632ab58fbec06ce926cb28c9d
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-07 10:08:58 +0630

    Update: comments pagination scrolll to top of comments section

--- resources/views/livewire/comment-feature/comments.blade.php
+++ resources/views/livewire/comment-feature/comments.blade.php
@@ -17,11 +17,15 @@
             @else
                 <a wire:navigate class="mt-2 text-sm" href="/login">Log in to comment!</a>
             @endauth
+
+            {{-- Showing Comments --}}
             @if ($comments->count())
-                @foreach ($comments as $comment)
-                    <livewire:comment-feature.comment :$comment :key="$comment->id" />
-                @endforeach
-                {{ $comments->links(data: ['scrollTo' => false]) }}
+                <div id="paginated-comments">
+                    @foreach ($comments as $comment)
+                        <livewire:comment-feature.comment :$comment :key="$comment->id" />
+                    @endforeach
+                    {{ $comments->links(data: ['scrollTo' => '#paginated-comments']) }}
+                </div>
             @else
                 <p>No comments yet!</p>
             @endif
